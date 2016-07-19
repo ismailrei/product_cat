974c28a7ab609676b427ed02bb850727479ea88b
ismailrei <ismailrei@gmail.com>
2016-07-19 18:06:34 +0100

    commit

---
+++ normal/lib/category.js
@@ -0,0 +1,53 @@
+var category=function (options){
+    var seneca=this;
+    seneca.add({role:"category",action:"add"},function(args,done){
+        var category=seneca.make("categories");
+        category.name=args.category.name;
+        category.description=args.category.description;
+        category.save$(function(err,cat)
+        {
+            done(err,category.data$(false));
+        });
+    })
+        .add({role:"category",action:"fetch"},function(args,done){
+            var categories=seneca.make("categories");
+            categories.list$({},done);
+        })
+        .add({role:"category",action:"fetch",critere:"byId"},function(args,done){
+            var categories=seneca.make("categories");
+            categories.load$(args.id,done);
+        })
+        .add({role:"category",action:"delete"},function(args,done){
+            var category=seneca.make("categories");
+            categories.remove(args.id,function(err)
+            {
+                donne(err,null);
+            });
+        })
+        .add({role:"category",action:"edit"},function(args,done){
+            seneca.act({role:"category",action:"fetch",critere:"byId",id:args.id},function(err,category)
+            {
+                category.data$(
+                    {
+                        name:args.name,
+                        description:args.description
+                    });
+                    category.save$(function(err,cat)
+                    {
+                        done(cat.data$(false));
+                    });
+            });
+        });
+}
+module.exports=category;
+
+var seneca=require("seneca")()
+    .use('entity')
+    .use(require('seneca-mongo-store'), {
+       name:"seneca",
+        host:"127.0.0.1",
+        port:"27017"
+       })
+    .use("category")
+    .listen({port:8080,
+            host:'127.0.0.1'});
---
+++ normal/lib/product.js
@@ -0,0 +1,69 @@
+var product=function(options)
+{
+    var seneca=this;
+    function searchCat(Id)
+    {
+        seneca.act({role:"category",action:"fetch",critere:"byId",id:Id},function(err,category)
+            {
+                return category;
+            });
+    }
+    seneca.add({role:"product",action:"add"},function(args,done){
+        var produit=seneca.make("products");
+        produit.name=args.product.name;
+        produit.description=args.product.description;
+        produit.category_id=args.product.category_id;
+        product.price=args.product.price;
+        produit.save$(function(err,prod){
+            done(null,produit.data$(false));
+        });
+    })
+        .add({role:"product",action:"fetch"},function(args,done)
+        {
+            var products=seneca.make("products");
+            /*products.list$({},function(err,list)
+            {
+                list.forEach(function(element) {
+                    var Category=searchCat(element.category_id);    
+                });
+            })*/
+            products.list$({},done);
+        })
+        .add({role:product,action:"fetch",critere:"byCategory"},function(args,done)
+        {
+            var products=seneca.make("products");
+           
+            products.list$({category_id:args.id},done);
+        })
+        .add({role:"product",action:"fetch",critere:"byId"},function(args,done){
+            var produit=seneca.make("products");
+            produit.load$(args.id,done);
+        })
+        .add({role:"product",action:"edit"},function(args,done)
+        {
+            seneca.act({role:"product",action:"fetch",critere:"byId",id:args.id},function(err,product){
+                product.data$({
+                    description:args.description,
+                    category_id:args.category_id,
+                    price:args.price
+                });
+                product.save$(function(err,prod)
+                 {
+                    done(prod.data$(false));
+                 });
+            });
+        });
+
+}
+module.exports=product;
+
+var seneca=require("seneca")()
+    .use('entity')
+    .use("product")
+    .use(require('seneca-mongo-store'), {
+       name:"seneca",
+        host:"127.0.0.1",
+        port:"27017"
+       })
+    .listen({port:8081,
+            host:'127.0.0.1'});
