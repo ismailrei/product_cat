eff797b2f9d32414d5825891c249e1df16cbe9d1
ismailrei <ismailrei@gmail.com>
2016-07-20 14:24:25 +0100

    commit

---
+++ expressV/lib/category.js
@@ -0,0 +1,56 @@
+var category=function (options){
+    var seneca=this;
+    seneca.add({role:"category",action:"add"},function(args,done){
+        var category=seneca.make("categories");
+        category.name=args.category.name;
+        category.description=args.category.description;
+        category.save$(function(err,cat)
+        {
+            done(err,category.data$(false));
+        });
+    })
+        .add({role:"category",action:"fetch"},function(args,done){
+            var categories=seneca.make("categories");
+            categories.list$({},done);
+        })
+        .add({role:"category",action:"fetch",critere:"byId"},function(args,done){
+            var categories=seneca.make("categories");
+            categories.load$(args.id,done);
+        })
+        .add({role:"category",action:"delete"},function(args,done){
+            var category=seneca.make("categories");
+            categories.remove(args.id,function(err)
+            {
+                donne(err,null);
+            });
+        })
+        .add({role:"category",action:"edit"},function(args,done){
+            //console.log("******************************");
+            seneca.act({role:"category",action:"fetch",critere:"byId",id:args.id},function(err,cat)
+            {
+               
+                cat.data$(
+                    {
+                        name:args.category.name,
+                        description:args.category.description
+                    });
+                     console.log(cat);
+                    cat.save$(function(err,cate)
+                    {
+                        done(cat.data$(false));
+                    });
+            });
+        });
+}
+module.exports=category;
+
+var seneca=require("seneca")()
+    .use('entity')
+    .use(require('seneca-mongo-store'), {
+       name:"seneca",
+        host:"127.0.0.1",
+        port:"27017"
+       })
+    .use("category")
+    .listen({port:8083,
+            host:'127.0.0.1'});
---
+++ expressV/test/index.js
@@ -0,0 +1,70 @@
+var senecaCategory = require("seneca")().client({host: "127.0.0.1",port: 8083})
+.use('entity');
+var senecaProduct = require("seneca")().client({host: "127.0.0.1",port: 8082})
+.use('entity');
+var seneca=require("seneca")()
+    seneca.use('entity')
+    seneca.use(require('seneca-mongo-store'), {
+       name:"seneca",
+        host:"127.0.0.1",
+        port:"27017"
+       })
+        
+        .ready(function(){
+             
+          var product=this.make$('product');
+          var cate1={
+              name:"category",
+              description:"first cat",
+          }
+          var prod1={
+              name:"xx",
+              description:"xx",
+              category_id:1,
+              price:14
+          }
+          var prod2={
+              name:"yy",
+              description:"yy",
+              
+              price:13
+          }
+          senecaCategory.act({role:"category",action:"fetch"},function(err,prod)
+          {
+             
+              console.log(prod);
+          });
+         // senecaCategory.act({role:"category",action:"edit",id:"578e4d39916657189a9e59b9",category:cate1},console.log);
+          /*senecaProduct.act({role:"product",action:"fetch",critere:"byCategory",id:"578e4d39916657189a9e59b9"},function(err,prod)
+          {
+              console.log(prod);
+          });
+          seneca.act({role:"category",action:"add",category:cate1},function(err,cat)
+          {
+              prod2.category_id=cat.id;
+            seneca.act({role:"product",action:"add",product:prod2},function(err,resp)
+          {
+              console.log(resp);
+          }) 
+          })
+           seneca.act({role:"product",action:"add",product:prod1},function(err,resp)
+          {
+              console.log(resp);
+          }) 
+         product.save$(function (err,prod1) {
+                product.load$({id: prod1.id}, function(err, foo) {
+                    console.log(foo);
+                });
+                 console.log( "prod.id = "+prod1.id  )
+                 })
+                 .save$(function (err,prod2) {
+                    
+                 console.log( "prod.id = "+prod2.id  )
+                 })        
+                 product.load$({id:"hawwbb"}, function (err, entity) {console.log(entity)});            
+            */  
+      })
+var app=require("express")()
+    .use(require("body-parser").json())
+    .use(seneca.export('web'))
+    .listen(3000);
